c0c1005304d4d3c2b5940faa665400499206f9c0
syncster31 <alexius.academia@gmail.com>
2017-11-05 12:25:00 +0800

    Create new work order feature created.

--- pages/new_work_order.js
+++ pages/new_work_order.js
@@ -28,6 +28,9 @@ db.getRows('app', { 'cat': 'settings' }, (succ, data) => {
   }
 });
 
+/**
+ * Retrieve customer vehicles
+ */
 function retrieveCustomerVehicles() {
   let vehicles = [];
   db.getRows(constant.vehiclesTable, { 'customer': selectedCustomer }, (succ, vehiclesList) => {
@@ -51,4 +54,73 @@ function retrieveCustomerVehicles() {
       }
     }
   });
+}
+
+$('#btn-create-work-order').on('click', () => {
+  let wo = new Object();  // Create work order object
+  let woDate = $('#date').val();
+
+  if (validateDate(woDate)) {
+    wo.customer = selectedCustomer;
+    wo.date = woDate;
+    wo.vehicle = $('#vehicle').val();
+
+    db.insertTableContent(constant.workordersTable, wo, (succ, msg) => {
+      if (!$('#err-alert').hasClass('hidden')) {
+        $('#err-alert').addClass('hidden');
+      }
+
+      $('#err-alert').removeClass('hidden');
+
+      if (succ) {
+        $('#err-alert').addClass('alert-success');
+        $('#err-alert').html("Work order created.");
+      } else {
+        $('#err-alert').addClass('alert-danger');
+        $('#err-alert').html("Error creating work order.");
+      }
+    });
+  } else {
+
+    return;
+  }
+  
+});
+
+$('#date').datepicker({});
+
+/**
+ * Validate date string format.
+ * @param {*} dateValue 
+ */
+function validateDate(dateValue)
+{
+    var selectedDate = dateValue;
+    if(selectedDate == '')
+        return false;
+
+    var regExp = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/; //Declare Regex
+    var dateArray = selectedDate.match(regExp); // is format OK?
+
+    if (dateArray == null){
+        return false;
+    }
+
+    month = dateArray[1];
+    day= dateArray[3];
+    year = dateArray[5];        
+
+    if (month < 1 || month > 12){
+        return false;
+    }else if (day < 1 || day> 31){ 
+        return false;
+    }else if ((month==4 || month==6 || month==9 || month==11) && day ==31){
+        return false;
+    }else if (month == 2){
+        var isLeapYear = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+        if (day> 29 || (day ==29 && !isLeapYear)){
+            return false
+        }
+    }
+    return true;
 }
