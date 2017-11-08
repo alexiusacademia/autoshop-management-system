aab8734bc057e7da76a0167da887517ca55065d3
syncster31 <alexius.academia@gmail.com>
2017-11-08 22:40:03 +0800

    Edit work order update feature.

--- pages/work_order_update.js
+++ pages/work_order_update.js
@@ -14,13 +14,29 @@ db.getRows(constant.appTable, {'cat': 'settings'}, (succ, appSettings) => {
   }
 })
 
+// Get the specified work order from the database
 db.getRows(constant.workordersTable, {'id': work_order_id}, (succ, workOrders) => {
   if (succ) {
     let wo = workOrders[0];
 
+    // Display date created
     $('#date-created').text(wo.date);
+
+    // Get the vehicle information
+    let vehicle;
+
+    db.getRows(constant.vehiclesTable, {'id': wo.vehicle}, (succ, rows) => {
+      if (succ) {
+        vehicle = rows[0];
+
+        // Display vehicle informations
+        $('#plate-number').html(vehicle.plate_number);
+        $('#vehicle').html(vehicle.make + " " + vehicle.model);
+      }
+    });
 
   } else {
     alert('Error loading work order data.');
   }
 });
+
