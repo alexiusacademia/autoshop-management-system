e172b306807d4693f5b4813a3acc327968655df7
syncster31 <alexius.academia@gmail.com>
2017-11-11 20:06:05 +0800

    Finished add work order feature.

--- pages/add_work_order_item.js
+++ pages/add_work_order_item.js
@@ -32,12 +32,63 @@ function insertItem(sender) {
 
   // Get the quantity
   let itemQuantity = $('#quantity-item-' + selectedItem).val();
+  itemQuantity = parseInt(itemQuantity);
 
   // Check if quantity is an integer
   if (Number.isInteger(parseInt(itemQuantity))) {
     // Get the stock
     let stock = getItemStock(selectedItem);
-    console.log(stock);
+
+    // Check if item is less than or equal to stock
+    if (itemQuantity > stock) {
+      alert('Item quantity entered is greater than the stock! Please review the quantity or add quantity on the item.');
+    } else {
+      // Create object
+      let itemObject = new Object();
+      itemObject.id = parseInt(selectedItem);
+      itemObject.quantity = itemQuantity;
+      itemObject.unit_price = getItemUnitPrice(selectedItem);
+
+      db.getRows(constant.appTable, { 'cat': 'settings' }, (succ, app) => {
+        if (succ) {
+          let selectedWorkOrder = app[0].selected_work_order;
+          let workOrder;
+          let items;
+          db.getRows(constant.workordersTable, { 'id': selectedWorkOrder }, (succ, workOrders) => {
+            if (succ) {
+              workOrder = workOrders[0];
+              if (workOrder.hasOwnProperty('items')) {
+                items = workOrder.items;
+                items.push(itemObject);
+                db.updateRow(constant.workordersTable, { 'id': selectedWorkOrder }, { 'items': items }, (succ, msg) => {
+                  if (succ) {
+                    // Go back to update work order view
+                    $('#main-container').load('pages/work_order_update.html', () => {
+
+                    });
+                  }
+                });
+              } else {
+                items = [];
+                items.push(itemObject);
+                db.updateRow(constant.workordersTable, { 'id': selectedWorkOrder }, { 'items': items }, (succ, msg) => {
+                  if (succ) {
+                    // Go back to update work order view
+                    $('#main-container').load('pages/work_order_update.html', () => {
+                      
+                    });
+                  }
+                });
+              }
+              reduceItemQuantity(selectedItem, itemQuantity);
+            }
+          });
+
+        } else {
+          alert('Error connecting to the app table from the database.');
+        }
+      });
+    }
   }
 }
 
@@ -50,16 +101,92 @@ function getItemStock(itemId) {
   let stock = 0;
   db.getRows(constant.itemsTable, { 'id': itemId }, (succ, items) => {
     if (succ) {
-      console.log('Item ID: ' + itemId);
-      console.log('Retrieving items...');
       if (items.length > 0) {
         let item = items[0];
         stock = item.item_quantity;
       } else {
-        console.log('No item with this id.');
         stock = 0;
       }
     }
   });
   return stock;
+}
+
+/**
+ * Function to get the selling price of the item
+ * @param {*} itemId 
+ */
+function getItemUnitPrice(itemId) {
+  itemId = parseInt(itemId);
+  let price = 0.0;
+
+  db.getRows(constant.itemsTable, { 'id': itemId }, (succ, items) => {
+    if (succ) {
+      if (items.length > 0) {
+        let item = items[0];
+        price = item.selling_price;
+      } else {
+        alert('There are no more stock for this item!');
+      }
+    }
+  });
+
+  return price;
+}
+
+/**
+ * Get the item name
+ * @param {*} itemId 
+ */
+function getItemName(itemId) {
+  itemId = parseInt(itemId);
+  let name = '';
+
+  db.getRows(constant.itemsTable, { 'id': itemId }, (succ, items) => {
+    if (succ) {
+      if (items.length > 0) {
+        let item = items[0];
+        name = item.item_name;
+      } else {
+        alert('The are no more stock for this item!');
+      }
+    } else {
+      alert('Error retrieving item name.');
+    }
+  });
+
+  return name;
+}
+
+/**
+ * Reducing item quantity from the database table
+ * @param {*} itemId 
+ * @param {*} deduction 
+ */
+function reduceItemQuantity(itemId, deduction){
+  itemId = parseInt(itemId);
+  let stock = getItemStock(itemId);
+  let newStock = stock - deduction;
+  let itemName = getItemName(itemId);
+  let itemUnitPrice = getItemUnitPrice(itemId);
+  // Update the item table
+  db.updateRow(constant.itemsTable, {'id': itemId}, {'item_quantity': newStock}, (succ, msg) => {
+    if (succ) {
+      // Insert record into transaction table, create new transaction
+      let transaction = {
+        'title': 'Subtracting item',
+        'description': itemName,
+        'expenses': 0.0,
+        'sales': itemUnitPrice * deduction,
+        'remarks': 'Subtract item quantity from inventory.'
+      };
+      db.insertTableContent('transactions', transaction, (succ, msg) => {
+        if (succ) {
+
+        } else {
+          alert(msg);
+        }
+      });
+    }
+  });
 }
